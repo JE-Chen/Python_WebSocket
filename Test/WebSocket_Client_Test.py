e52cbcce0597ae18a5eafaa72bea98bde8c7bd6a
JE-Chen <zenmailman@gmail.com>
2020-11-27 20:41:37 +0800

    test

--- Test/WebSocket_Client_Test.py
+++ Test/WebSocket_Client_Test.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.append("/Python_Websocket/Module/")
+sys.path.append("/Python_Websocket/")
 from Module.WebSocket_Client import WebSocket_Client
 Client = WebSocket_Client('ws://localhost:5555')
