4a80ea37be7f65e94839a2d7bd14b5d6f12bda6f
JE-Chen <zenmailman@gmail.com>
2020-11-28 04:39:24 +0800

    test

--- Test/Websocket_Server_Test.py
+++ Test/Websocket_Server_Test.py
@@ -1,3 +1,5 @@
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__),os.pardir,"Python_WebSocket"))
 from Python_WebSocket import WebSocket_Server
 
 Server = WebSocket_Server("localhost", 5555)
