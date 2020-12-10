1bd8294f603e03a06d132dda65c834dbc078f625
JE-Chen <zenmailman@gmail.com>
2020-12-10 22:30:05 +0800

    new ver
    new ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="JEWebSocket",
-    version="0.0.0.0.1",
+    version="0.0.0.0.2",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="JE use WebSocket",
